94614236d2bffc5cc5a1daf4865ccfb590566689
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2020-01-19 11:59:48 -0600

    created a function that prints a word and used it in 3 different setTimeout functions

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 function msg(message) {
   console.log(message);
 }
-msg('Hello World!');
+// msg('Hello World!');
 
-setTimeout(function() {msg('Hidy ho, neighbor')}, 3000);
+// setTimeout(function() {msg('Hidy ho, neighbor')}, 3000);
 
-function getWords()
+function getWords(word) {
+  console.log(word);
+}
+// getWords('Stadium');
+// setTimeout(function() {getWords('Chiefs')}, 3000);
+// setTimeout(function() {getWords('Wings')}, 2000);
+// setTimeout(function() {getWords('Foosball')}, 1000);
+
+function done() {
+  console.log(`Job's done!`);
+}
+done();
